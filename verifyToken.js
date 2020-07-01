6fbdf93625efc859586e5b783ca9cda1042206f2
guladishvili-exceed <n.guladishvili.exceedteam@gmail.com>
2020-07-01 18:42:46 +0400

    commit

---
+++ verifyToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+	const token = req.header("auth-token")
+	//If token is not presented
+	if(!token) {
+		return res.status(401).send('Access Denied')
+	}
+	try {
+		const verified = jwt.verify(token,"secretKey")
+		req.user = verified;
+		next();
+	} catch (err) {
+		res.status(400).send("Invalid Token")
+		next();
+	}
+}
